0ee199a6b8256977fec4100e2afd60b10b87d778
youssef <youssef.sahri@capgemini.com>
2019-04-17 12:25:52 +0200

    hello louise test

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	} else {
 		name = "world"
 	}
-	fmt.Fprintf(w, "Hello youssef %s!", name)
+	fmt.Fprintf(w, "Hello louise %s!", name)
 }
 
 func main() {
